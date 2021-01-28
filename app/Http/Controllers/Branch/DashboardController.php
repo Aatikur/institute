bb6e871fac9daab1324ec05e12e31501c1debd19
Aatikur <atikrex@gmail.com>
2021-01-28 17:39:57 +0530

    u

--- app/Http/Controllers/Branch/DashboardController.php
+++ app/Http/Controllers/Branch/DashboardController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Branch;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Student;
+use App\Models\StudentDetail;
+use Auth;
 class DashboardController extends Controller
 {
     public function dashboardView()
     {
-        return view('branch.dashboard');
+        $student = StudentDetail::where('branch_id',Auth::user()->id)->latest()->limit(10)->get();
+        $student_cnt = Student::where('branch_id',Auth::user()->id)->count();
+        return view('branch.dashboard',compact('student_cnt','student'));
     }
 }
--- resources/views/branch/dashboard.blade.php
+++ resources/views/branch/dashboard.blade.php
@@ -4,12 +4,12 @@
 
 <div class="right_col" role="main">
      <!-- top tiles -->
-     {{-- <div class="row tile_count">
+    <div class="row tile_count">
       <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count" style="text-align: center">
-        <span class="count_top"><i class="fa fa-user"></i> Total Customers</span>
-        <div class="count green">10</div>
+        <span class="count_top"><i class="fa fa-user"></i> Total Students</span>
+        <div class="count green">{{$student_cnt}}</div>
       </div>
-      <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count"  style="text-align: center">
+      {{--<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count"  style="text-align: center">
         <span class="count_top"><i class="fa fa-clock-o"></i> Total Retailers</span>
         <div class="count green">10</div>
       </div>
@@ -24,17 +24,17 @@
       <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count"  style="text-align: center">
         <span class="count_top"><i class="fa fa-user"></i> Total New Orders</span>
         <div class="count green">10</div>
-      </div>
+      </div>--}}
       
-    </div> --}}
+    </div> 
     <!-- /top tiles -->
 
   <div class="row">
 
-    {{-- <div class="col-md-12">
+    <div class="col-md-12">
       <div class="x_panel">
         <div class="x_title">
-            <h2>Category List</h2>
+            <h2>Last 10 Students</h2>
             <div class="clearfix"></div>
         </div>
         <div>
@@ -44,25 +44,21 @@
               <thead>
                 <tr>
                     <th>SL No.</th>
-                    <th>Slot Number</th>
-                    <th>Number</th>
-                    <th>Action</th>
+                    <th>Student Name</th>
+                    <th>Father Name</th>
+                    <th>Date Of Birth</th>
+                    <th>Enrollment Id</th>
                 </tr>
               </thead>
               <tbody class="form-text-element">
-                  @if (isset($slots) && !empty($slots))
-                    @php
-                        $count = 1;
-                    @endphp
-                      @foreach ($slots as $item)
+                  @if (isset($student) && !empty($student))
+                      @foreach ($student as $item)
                         <tr>
-                          <td>{{$count++}}</td>
-                          <td>{{$item->slot_no}}</td>
-                          <td>{{$item->number}}</td>
-                          <td>
-                          <a class="btn btn-warning" href="{{route('admin.slot_edit',['slot_id'=>$item->id])}}">Edit</a>
-                          <a class="btn btn-danger" href="{{route('admin.slot_delete',['slot_id'=>$item->id])}}">Delete</a>
-                          </td>
+                          <td>{{$loop->iteration}}</td>
+                          <td>{{$item->name}}</td>
+                          <td>{{$item->father_name}}</td>
+                          <td>{{$item->dob}}</td>
+                          <td>{{$item->student->enrollment_id??null}}</td>
                         </tr>
                       @endforeach
                   @endif
@@ -70,9 +66,10 @@
             </table>
             </div>
           </div>
-        </div>
+            <a class="btn btn-primary btn-sm" href="{{route('branch.student_list')}}">View More</a>
+         </div>
       </div>
-    </div> --}}
+    </div>
   </div>
 </div>
  @endsection
