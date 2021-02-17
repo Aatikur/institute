21a00765963cfd79e9a655ce5488880b0b09ef5c
Aatikur <atikrex@gmail.com>
2021-02-17 18:03:16 +0530

    u

--- resources/views/admin/branch/edit_branch_form.blade.php
+++ resources/views/admin/branch/edit_branch_form.blade.php
@@ -36,7 +36,7 @@
                         <div class="well" style="overflow: auto">
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="branch_email">Branch Email<span><b style="color: red"> * </b></span></label>
-                                <input type="email" class="form-control" value="{{ $branch->email }}" name="branch_email" disabled  >
+                                <input type="email" class="form-control" value="{{ isset($branch->email)?$branch->email:'' }}" name="branch_email" disabled  >
                                 @if($errors->has('branch_email'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('branch_email') }}</strong>
@@ -45,7 +45,7 @@
                             </div>
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="branch_mobile">Branch Mobile<span><b style="color: red"> * </b></span></label>
-                                <input type="tel" class="form-control" value="{{ $branch->mobile }}" name="branch_mobile"   >
+                                <input type="tel" class="form-control" value="{{ isset($branch->mobile)?$branch->mobile:'' }}" name="branch_mobile"   >
                                 @if($errors->has('branch_mobile'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('branch_mobile') }}</strong>
@@ -65,7 +65,7 @@
                             </div>
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="cnt_email">Contact Person Email<span><b style="color: red"> * </b></span></label>
-                                <input type="email" class="form-control" value="{{ $branch_data->email_address }}" name="cnt_email"   >
+                                <input type="email" class="form-control" value="{{ isset($branch_data->email_address)?$branch_data->email_address:'' }}" name="cnt_email"   >
                                 @if($errors->has('cnt_email'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('cnt_email') }}</strong>
@@ -75,7 +75,7 @@
                             
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="cnt_state">Contact Person State<span><b style="color: red"> * </b></span></label>
-                                <input type="text" class="form-control" value="{{ $branch_data->residence_address }}" name="cnt_state"   >
+                                <input type="text" class="form-control" value="{{ isset($branch_data->residence_address)?$branch_data->residence_address:'' }}" name="cnt_state"   >
                                 @if($errors->has('cnt_state'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('cnt_state') }}</strong>
@@ -84,7 +84,7 @@
                             </div>
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="cnt_city">Contact Person City<span><b style="color: red"> * </b></span></label>
-                                <input type="text" class="form-control" value="{{ $branch_data->city }}" name="cnt_city"   >
+                                <input type="text" class="form-control" value="{{ isset($branch_data->city)?$branch_data->city:'' }}" name="cnt_city"   >
                                 @if($errors->has('cnt_city'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('cnt_city') }}</strong>
@@ -93,7 +93,7 @@
                             </div>
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="cnt_dob">Contact Person Date Of Birth<span><b style="color: red"> * </b></span></label>
-                                <input type="date" class="form-control" name="cnt_dob"  value="{{ old('cnt_dob') }}" value="{{ $branch_data->dob }}" >
+                                <input type="date" class="form-control" name="cnt_dob"  value="{{ isset($branch_data->dob)?$branch_data->dob:'' }}" >
                                 @if($errors->has('cnt_dob'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('cnt_dob') }}</strong>
@@ -102,7 +102,7 @@
                             </div>
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="cnt_qctn">Contact Person Qualification<span><b style="color: red"> * </b></span></label>
-                                <input type="text" class="form-control" name="cnt_qctn"   value="{{ $branch_data->qualification }}" >
+                                <input type="text" class="form-control" name="cnt_qctn"   value="{{ isset($branch_data->qualification)?$branch_data->qualification:'' }}" >
                                 @if($errors->has('cnt_qctn'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('cnt_qctn') }}</strong>
@@ -112,7 +112,7 @@
                             <div class="form-row mb-10">
                                 <div class="col-md-12 col-sm-12 col-xs-12 mb-3">
                                     <label for="cnt_address" >Contact Person Residence Address <span><b style="color: red"> * </b></span></label>
-                                    <textarea class="form-control" rows="4" name="cnt_address">{{ $branch_data->residence_address }}</textarea>
+                                    <textarea class="form-control" rows="4" name="cnt_address">{{ isset($branch_data->residence_address)?$branch_data->residence_address:'' }}</textarea>
                                     @if($errors->has('cnt_address'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('cnt_address') }}</strong>
@@ -124,7 +124,7 @@
                         <div class="well" style="overflow: auto">
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="center_name">Center Name<span><b style="color: red"> * </b></span></label>
-                                <input type="text" class="form-control" name="center_name" value="{{ $branch_data->center_name }}" >
+                                <input type="text" class="form-control" name="center_name" value="{{ isset($branch_data->center_name)?$branch_data->center_name:'' }}" >
                                 @if($errors->has('center_name'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('center_name') }}</strong>
@@ -133,7 +133,7 @@
                             </div>
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="center_city">Center City<span><b style="color: red"> * </b></span></label>
-                                <input type="text" class="form-control" name="center_city"  value="{{ $branch_data->center_city }}" >
+                                <input type="text" class="form-control" name="center_city"  value="{{ isset($branch_data->center_city)?$branch_data->center_city:'' }}" >
                                 @if($errors->has('center_city'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('center_city') }}</strong>
@@ -142,7 +142,7 @@
                             </div> 
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="center_state">Center State<span><b style="color: red"> * </b></span></label>
-                                <input type="text" class="form-control" name="center_state"  value="{{ $branch_data->center_state }}" >
+                                <input type="text" class="form-control" name="center_state"  value="{{ isset($branch_data->center_state) ?$branch_data->center_state:''}}" >
                                 @if($errors->has('center_state'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('center_state') }}</strong>
@@ -151,7 +151,7 @@
                             </div> 
                             <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                 <label for="center_district">Center District<span><b style="color: red"> * </b></span></label>
-                                <input type="text" class="form-control" name="center_district"   value="{{ $branch_data->center_district }}">
+                                <input type="text" class="form-control" name="center_district"   value="{{isset($branch_data->center_district)?$branch_data->center_district:''}}">
                                 @if($errors->has('center_district'))
                                     <span class="invalid-feedback" role="alert" style="color:red">
                                         <strong>{{ $errors->first('center_district') }}</strong>
@@ -161,7 +161,7 @@
                             <div class="form-row mb-10">
                                 <div class="col-md-12 col-sm-12 col-xs-12 mb-3">
                                     <label for="center_address" >Center Address <span><b style="color: red"> * </b></span></label>
-                                    <textarea class="form-control" rows="4" name="center_address">{{ $branch_data->center_district }} </textarea>
+                                    <textarea class="form-control" rows="4" name="center_address">{{ isset($branch_data->center_district)?$branch_data->center_district:'' }} </textarea>
                                     @if($errors->has('center_address'))
                                         <span class="invalid-feedback" role="alert" style="color:red">
                                             <strong>{{ $errors->first('center_address') }}</strong>
@@ -215,7 +215,7 @@
                                     <label for="pan_photo" >PAN Card Photo<span><b style="color: red"> * </b></span></label>
                                     <input type="file" class="form-control" name="pan_photo">
                                     @if(!empty($branch_data->pan_card))
-                                        <img src="{{asset('images/docs/pan/'.$branch_data->pan_card)}}" style="width:150px;height:150px;">
+                                        <img src="{{asset('images/docs/pan/'.isset($branch_data->pan_card)?$branch_data->pan_card:'')}}" style="width:150px;height:150px;">
                                     @endif
                                     @if($errors->has('pan_photo'))
                                         <span class="invalid-feedback" role="alert" style="color:red">
@@ -229,7 +229,7 @@
                                     <label for="hs_photo" >H.S Certificate Photo<span><b style="color: red"> * </b></span></label>
                                     <input type="file" class="form-control" name="hs_photo">
                                     @if(!empty($branch_data->hs_certificate))
-                                        <img src="{{asset('images/docs/trade/'.$branch_data->hs_certificate)}}" style="width:150px;height:150px;">
+                                        <img src="{{asset('images/docs/trade/'.isset($branch_data->hs_certificate)?$branch_data->hs_certificate:'')}}" style="width:150px;height:150px;">
                                     @endif
                                     @if($errors->has('hs_photo'))
                                         <span class="invalid-feedback" role="alert" style="color:red">
